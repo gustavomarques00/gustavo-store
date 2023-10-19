1444e3a35f056b59a8074ec3546ca4b8e716a1ff
Gustavo <gustavomarquesmm@gmail.com>
2023-10-19 15:09:32 -0300

    feat: add Categoies and CategoriesItem component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
       </Sheet>
 
       <h1 className="text-lg font-semibold">
-        <span className="text-primary">FSW</span> Store
+        <span className="text-primary">GUSTAVO</span> Store
       </h1>
 
       <Button size="icon" variant="outline">
